53e52ad1005f24d7fa66e0761438ea0332275098
e.andreev <k9prP46sXJ>
2022-12-12 17:02:53 +0300

    Gets permissions

--- app/src/main/java/com/altertravel/MapsActivity.kt
+++ app/src/main/java/com/altertravel/MapsActivity.kt
@@ -1,68 +1,116 @@
 package com.altertravel
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.altertravel.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.Marker
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+class MapsActivity : AppCompatActivity(),
+    OnMapReadyCallback,
+    LocationListener,
+    GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener,
+    ActivityCompat.OnRequestPermissionsResultCallback,
+    GoogleMap.OnCameraMoveListener {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
     private lateinit var locationManager: LocationManager
     private lateinit var currentLocation: LatLng
     private lateinit var currentLocationMarker: Marker
-    private val locationPermissionCode = 2
+    private lateinit var mapFragment: MapFragment
+    private var firstFixDone = false
+    private var TAG = "MainAct"
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_BACKGROUND_LOCATION, false) -> {
+                    Log.d(TAG, "Background location access granted.")
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d(TAG, "Precise location access granted.")
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d(TAG, "Only approximate location access granted.")
+                }
+                else -> {
+                    Log.d(TAG, "No location access granted.")
+                }
+            }
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+            enableMap()
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        )
+
+        Log.d(TAG, "onCreate")
+    }
+
+    private fun enableMap() {
+        Log.d(TAG, "enableMap")
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.map, mapFragment)
+            .commit()
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        currentLocationMarker = mMap.addMarker(MarkerOptions().position(LatLng(0.0,0.0)).title("Me"))!!
-
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if ((ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED)
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                locationPermissionCode
-            )
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5f, this)
+        Log.d(TAG, "onMapReady")
     }
 
     override fun onLocationChanged(location: Location) {
+        Log.d(TAG, "onLocationChanged")
         currentLocation = LatLng(location.latitude, location.latitude)
-        currentLocationMarker.setPosition(currentLocation)
+        if (!firstFixDone) {
+            currentLocationMarker = mMap.addMarker(
+                MarkerOptions()
+                    .position(currentLocation)
+                    .title("Me")
+            )!!
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+            firstFixDone = true
+        }
+        currentLocationMarker.position = currentLocation
+    }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+    override fun onCameraMove() {
+        Log.d(TAG, "onCameraMove")
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Log.d(TAG, "onMyLocationButtonClick")
+        return true
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Log.d(TAG, "onMyLocationClick")
     }
 }
